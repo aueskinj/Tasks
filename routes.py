4158dd3589fd48bd53bd0d2e793c9d7b1e50c511
aueskinj <aueskinj@gmail.com>
2023-05-23 11:35:38 +0000

    fixed some few things got one more errot

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from models import Task
 from datetime import datetime
 
@@ -8,14 +8,27 @@ import forms
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', current_title='Custom Title')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks = tasks)
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
+@app.route('/add', methods=['GET', 'POST'])
+def add():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
         t = Task(title=form.title.data, date=datetime.utcnow())
         db.session.add(t)
         db.session.commit()
+        flash('Task added to the database')
         return redirect(url_for('index'))
-    return render_template('about.html', form=form)
+    return render_template('add.html', form=form)
+
+#passes info to the route functions, edit has function to task id
+@app.route('/edit/<int:task_id>')
+def edit(task_id):
+    task = Task.query.get(task_id)
+    form = forms.AddTaskForm()
+    print(task)
+    if task:
+        form.title.data = task.title
+        return render_template('edit.html', form=form)
+    return redirect(url_for('index'))
